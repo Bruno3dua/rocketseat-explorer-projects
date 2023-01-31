fdce19fd5d9078a26577003b4e703d88e0b8ccc0
bruno <brunoedua21@gmail.com>
2023-01-31 09:34:12 -0400

    Adicionando params e query

--- src/server.js
+++ src/server.js
@@ -2,10 +2,20 @@ const express = require('express')
 
 const app = express()
 
+//usando parâmetros
 app.get('/message/:id/:user', (request, response) => {
     response.send(`
     Id da mensagem: ${request.params.id}.
     e o usuário: ${request.params.user}.
+    `)
+})
+
+//usando query
+app.get('/users', (request, response) => {
+    const { page, limit } = request.query
+    response.send(`
+    Página: ${page}.
+    Limite: ${limit}.
     `)
 })
 
